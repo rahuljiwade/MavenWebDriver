c3770fe968707ba6fa9cf65efdfae29cf98d78bc
rahuljiwade <rahuljiwade001@gmail.com>
2020-06-02 18:12:43 +0530

    New Test class is added

---
+++ src/main/java/Sample/com/TestGit/Test1.java
@@ -0,0 +1,13 @@
+package Sample.com.TestGit;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	
+	@Test
+	public void test() {
+		System.out.println("This is the test method of Test1 class");
+	}
+
+}
