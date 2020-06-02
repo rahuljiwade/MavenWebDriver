155fa4f27c6cfdeb5db5e8c5bdce16403e735a87
rahuljiwade <rahuljiwade001@gmail.com>
2020-06-02 19:01:19 +0530

    Added java program

--- src/main/java/Sample/com/TestGit/Test1.java
+++ src/main/java/Sample/com/TestGit/Test1.java
@@ -1,6 +1,15 @@
 package Sample.com.TestGit;
 
+import java.util.Arrays;
+
 import org.testng.annotations.Test;
+/*
+ * input-->> String s="01/06/2020";
+ * output-->>01
+ *           06
+ *           2020
+ * 
+ */
 
 public class Test1 {
 	
@@ -8,6 +17,19 @@ public class Test1 {
 	@Test
 	public void test() {
 		System.out.println("This is the test method of Test1 class");
+		
+		String s="01/06/2020";
+		
+		//First method
+		String[] st=s.split("/");
+		
+		for(String s1:st) {
+			System.out.println(s1);
+		}
+		
+		//Second method
+		Arrays.asList(s.split("/")).stream().forEach(System.out::println);
+		
 	}
 
 }
